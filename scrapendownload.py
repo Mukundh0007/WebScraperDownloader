47415fe09bb02ce0f984b58de75a76aacfc66d42
Mukundh J <93877999+Mukundh0007@users.noreply.github.com>
2023-10-24 14:32:51 +0530

    Update scrapendownload.py

--- scrapendownload.py
+++ scrapendownload.py
@@ -63,7 +63,8 @@ def download_image(download_path, url, file_name):
         print("Success")
     except Exception as e:
         file_path = download_path + file_name
-        os.remove(file_path)
+        try:os.remove(file_path)
+        except:pass
         print('FAILED -', e)
 
 query=input()
